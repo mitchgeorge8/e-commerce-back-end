bef5a211b54d04be2b955fde1d357ed464b14d96
Mitchell George <mitchgeorge8@gmail.com>
2022-05-29 00:33:09 -0400

    Created models and server sync to database

--- models/index.js
+++ models/index.js
@@ -1,16 +1,34 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "SET NULL",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "tagged_product",
+  foreignKey: "product_id",
+  onDelete: "SET NULL",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "tagged_product",
+  foreignKey: "tag_id",
+  onDelete: "SET NULL",
+});
 
 module.exports = {
   Product,
